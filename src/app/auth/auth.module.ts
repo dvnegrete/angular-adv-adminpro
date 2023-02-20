e4dc2c4a64e17b828f3c6f4ca79cc0e0613146a8
Damian V.N <dvnegrete@gmail.com>
2023-02-19 22:37:26 -0600

    Fin Seccion 4 - Router

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register/register.component';
-import { NotFoundPagesComponent } from '../not-found-pages/not-found-pages.component';
+import { NotFoundPagesComponent } from '../404not-found-pages/not-found-pages.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     RegisterComponent,
+    LoginComponent,
     NotFoundPagesComponent
   ],
   imports: [
